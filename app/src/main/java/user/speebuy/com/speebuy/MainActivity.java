9b1e7f9f305bec642817df853ae786cc4e0e37aa
Pranav <pranavmadavadev78@gmail.com>
2018-07-28 17:00:06 +0530

    first commit

---
+++ app/src/main/java/user/speebuy/com/speebuy/MainActivity.java
@@ -0,0 +1,20 @@
+package user.speebuy.com.speebuy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getShops();
+    }
+    void getShops()
+    {
+
+
+    }
+}
